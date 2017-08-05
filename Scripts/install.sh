f910bf5ab34f8e3fd35f5165e8e7a990a6335218
cwolfe007 <cwolfe651@gmail.com>
2017-08-05 00:14:36 -0500

    printing env var

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+printenv $DEPLOYMENT_GROUP_ID;
+printenv $DEPLOYMENT_ID;
 cd $DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/myTestApp;
 pwd;
 /home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm install
