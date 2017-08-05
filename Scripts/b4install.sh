05303542ed0bc9890da5e17eeba54af201b4e534
cwolfe007 <cwolfe651@gmail.com>
2017-08-05 09:50:32 -0500

    cleaning up folder and re-installing

--- Scripts/b4install.sh
+++ Scripts/b4install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+\rm -rf /home/ec2-user/web/ && mkdir /home/ec2-user/web/
 echo "Before install complete";
--- Scripts/stop.sh
+++ Scripts/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#cd /home/ec2-user/web/myTestApp;pwd;/home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm stop;
+
 echo "ran stop"
