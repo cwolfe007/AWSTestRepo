a49bbbce9f89c85dab8c7762bcf06de07bf93586
cwolfe007 <cwolfe651@gmail.com>
2017-08-05 00:30:24 -0500

    will this one work?

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-echo "starting install";
-printenv $DEPLOYMENT_GROUP_ID;
-echo "starting install";
-printenv $DEPLOYMENT_ID;
-echo "starting install";
-cd $DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/myTestApp;
+cd /tmp/epub/myTestApp
 pwd;
 /home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm install
