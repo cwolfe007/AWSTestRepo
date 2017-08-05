7dfc399e50c6d1eb6f5372824ee05a7513b2e96b
cwolfe007 <cwolfe651@gmail.com>
2017-08-05 00:23:31 -0500

    printing echos to test

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+echo "starting install";
 printenv $DEPLOYMENT_GROUP_ID;
+echo "starting install";
 printenv $DEPLOYMENT_ID;
+echo "starting install";
 cd $DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/myTestApp;
 pwd;
 /home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm install
