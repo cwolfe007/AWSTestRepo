d1323f000a48ccfc824bc71835f58ececaf567de
cwolfe007 <cwolfe651@gmail.com>
2017-08-04 23:14:09 -0500

    modifying the cd command adding location of bashscript, learned that bash runs in its own shell

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cd ~/../myTestApp
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd DIR 
+echo pwd 
+cd ../myTestApp
 /home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm install
