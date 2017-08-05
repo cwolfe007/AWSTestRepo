0f35c9614c68da65fce5be22c7e142fd146fa237
cwolfe007 <cwolfe651@gmail.com>
2017-08-04 23:36:54 -0500

    got rid of destination and changed the way I find the home directory

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd DIR 
+echo DIR
 pwd 
-cd ../myTestApp
+cd .myTestApp
 pwd
 /home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm install
