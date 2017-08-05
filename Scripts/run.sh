1e2be33f76fd25416d6ccf9909a06c352501d03a
cwolfe007 <cwolfe651@gmail.com>
2017-08-05 07:52:47 -0500

    will this one work?

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$ cd  $DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/myTestApp;
-$ /home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm start
+cd  /tmp/epub/;
+/home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm start
--- Scripts/validation.sh
+++ Scripts/validation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+cd /tmp/epub/myTestApp
 echo "validation run, no real tests executed"
