eeb946019ed21352099a6c9e81e24a49e82bebaa
cwolfe007 <cwolfe651@gmail.com>
2017-08-06 20:57:28 -0500

    :) modified run.sh again

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/web/myTestApp/;pwd;npm install;npm start;
+cd /home/ec2-user/web/myTestApp;pwd;/home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm install;/home/ec2-user/.nvm/versions/node/v4.4.5/bin/npm start;
