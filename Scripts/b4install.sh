d7b0819a10f61cb37d3c07351a0e8f1e2afa66b2
cwolfe007 <cwolfe651@gmail.com>
2017-08-08 23:13:24 -0500

    Changed BeforeInstall to run as ec2 for install and root for symlink creation

--- Scripts/b4install.sh
+++ Scripts/b4install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-\rm -rf /home/ec2-user/web/ && mkdir /home/ec2-user/web/;ln -s /usr/bin/nodejs /usr/bin/node;
+\rm -rf /home/ec2-user/web/ && mkdir /home/ec2-user/web/;chsh -s /bin/bash ec2-user;curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash;. ~/.nvm/nvm.sh;nvm install 4.4.5;node -e "console.log('Running Node.js ' + process.version)";echo "Before install complete";chsh -s /bin/bash root;ln -s /usr/bin/nodejs /usr/bin/node;
