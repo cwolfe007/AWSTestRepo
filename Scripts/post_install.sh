f7ad26109fd683ef19910a35f33d0878f815b70c
cwolfe007 <cwolfe651@gmail.com>
2017-08-10 21:41:28 -0500

    removed custom paths from the run.sh, since running as ec2-user in app spec

--- Scripts/post_install.sh
+++ Scripts/post_install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 800;echo "post install";
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000;echo "post install";
