cf71e0412787223dca499e971da9813e1c221540
cwolfe007 <cwolfe651@gmail.com>
2017-08-10 22:18:51 -0500

    added printenv to test

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/web/myTestApp;pwd;$NVM_BIN/npm install -g forever;$NVM_BIN/forever start ./bin/www;printenv
+cd /home/ec2-user/web/myTestApp;printenv;$NVM_BIN/npm install -g forever;$NVM_BIN/forever start ./bin/www;printenv
