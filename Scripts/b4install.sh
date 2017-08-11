59c0558ebd47c2aa845b5e84722933c0b80a812e
cwolfe007 <cwolfe651@gmail.com>
2017-08-10 20:39:09 -0500

    changes the symlink test in b4install, which has the conditional referenced in the previous commit

--- Scripts/b4install.sh
+++ Scripts/b4install.sh
@@ -2,7 +2,7 @@
 DIRECTORY = /usr/bin/node
 
 chsh -s /bin/bash ec2-user;curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash;. ~/.nvm/nvm.sh;nvm install 4.4.5;node -e "console.log('Running Node.js ' + process.version)";echo "Before install complete";chsh -s /bin/bash root;
-if [ ! -d "$DIRECTORY" ]; then
+if [ ! -L $DIRECTORY ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   ln -s /usr/bin/nodejs /usr/bin/node;
 fi
