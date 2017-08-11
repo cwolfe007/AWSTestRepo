1d4db2eac0c5865747debf1cfdb987bdd3f7699e
cwolfe007 <cwolfe651@gmail.com>
2017-08-10 22:10:54 -0500

    added  to complete path calls for npm and forever

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/web/myTestApp;pwd;npm install -g forever;forever start ./bin/www;
+cd /home/ec2-user/web/myTestApp;pwd;$PATH/npm install -g forever;$PATH/forever start ./bin/www;
