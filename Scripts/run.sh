94f8a904a75bad0dc3b427c1f754ff0912c7067d
cwolfe007 <cwolfe651@gmail.com>
2017-08-12 11:55:57 -0500

    added printenv statement to see the value of NVM_BIN

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/web/myTestApp;$NVM_BIN/npm install -g forever;$NVM_BIN/forever start ./bin/www;
+cd /home/ec2-user/web/myTestApp;printenv | grep $NVM_BIN;$NVM_BIN/npm install -g forever;$NVM_BIN/forever start ./bin/www;
