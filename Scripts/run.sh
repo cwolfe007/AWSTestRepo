ac8d020b2e55245d124cc92813bb6eef85548c99
cwolfe007 <cwolfe651@gmail.com>
2017-08-12 21:20:50 -0500

    checking shell

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/web/myTestApp;printenv;$NVM_BIN/npm install -g forever;$NVM_BIN/forever start ./bin/www;
+cd /home/ec2-user/web/myTestApp;printenv | grep SHELL;$NVM_BIN/npm install -g forever;$NVM_BIN/forever start ./bin/www;
